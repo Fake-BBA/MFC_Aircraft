923670647e86e540864a8f34dc2308a2783d53bc
Fake-BBA <401131019@qq.com>
2019-03-24 16:21:30 +0800

    save

--- Smart_Home/BBA_FlightStatus.cpp
+++ Smart_Home/BBA_FlightStatus.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "BBA_FlightStatus.h"
+#include "Resource.h"
 
 BBA_FlightStatus::BBA_FlightStatus()
 {
@@ -21,6 +22,7 @@ BEGIN_MESSAGE_MAP(BBA_FlightStatus, BBA_CWnd)
 	ON_WM_PAINT()
 	ON_WM_SIZE()
 	ON_WM_CREATE()
+	ON_WM_HSCROLL()	//滑条消息
 END_MESSAGE_MAP()
 
 void BBA_FlightStatus::OnSize(UINT nType, int cx, int cy)
@@ -35,11 +37,27 @@ void BBA_FlightStatus::OnPaint()
 	this->GetClientRect(&m_RectClientWindow);
 
 	m_pPDC = new CPaintDC(this);
+	//输出飞行器姿态信息
 	for (int i = 0; i < textLineNum; i++)
 	{
 		for (int j = 0; j < textListNum; j++)
 		{
-			m_pPDC->TextOut(textPoint[i][j].x, textPoint[i][j].y, "Hello");
+			m_pPDC->TextOut(textPoint[i][j].x, textPoint[i][j].y, textStr[i][j]);
+		}
+	}
+	//绘制滑动条名称
+	CPoint point;
+	CString temp;
+	
+	for (int i = 0; i < sliderLineNum; i++)
+	{
+		for (int j = 0; j < sliderListNum; j++)
+		{
+			temp.LoadStringA(IDS_STRING_STATIC_PITCH+i*sliderListNum+j);
+			point = sliderPoint[i][j];
+			point.y = point.y + sliderWidth / 2 - 8;	//输出在中间
+			point.x = point.x - 50;	//输出起点在滑条前50个像素点
+			m_pPDC->TextOut(point.x, point.y, temp);
 		}
 	}
 }
@@ -64,18 +82,20 @@ int BBA_FlightStatus::OnCreate(LPCREATESTRUCT lpCreateStruct)
 		{
 			textPoint[i][j].x = textStartPoint.x+ i * textListInterval;
 			textPoint[i][j].y = textStartPoint.y+j * textLineInterval;
+			memcpy(textStr[i][j], "Hello", 6);
 		}
 	}
 
 	CString temp;
 	CRect rect;
-	sliderLineNum = 2;	//每一行有2个slider
-	sliderListNum = 10;	//每一列有10个slider
+	sliderLineNum = 1;	//每一行有1个slider
+	sliderListNum = 12;	//每一列有10个slider
 	sliderLineInterval = 50;	//每一行slider的间隔
 	sliderListInterval = 300;	//每一列slider的间隔
-	sliderStartPoint.x = m_RectClientWindow.right/2;	//slider矩阵的起点x
-	sliderStartPoint.y = 100;	//slider矩阵的起点y
-
+	sliderStartPoint.x = m_RectClientWindow.right-300;	//slider矩阵的起点x
+	sliderStartPoint.y = 20;	//slider矩阵的起点y
+	sliderLength = 250;		//滑条长
+	sliderWidth = 30;		//滑条高
 	for (int i = 0; i < sliderLineNum; i++)
 	{
 		for (int j = 0; j < sliderListNum; j++)
@@ -85,9 +105,46 @@ int BBA_FlightStatus::OnCreate(LPCREATESTRUCT lpCreateStruct)
 
 			rect.left = sliderPoint[i][j].x;
 			rect.top = sliderPoint[i][j].y;
-			rect.bottom = rect.top + 30;	//滑条的高
-			rect.right = rect.left + 200;	//滑条的长
+			
+			rect.bottom = rect.top + sliderWidth;	//滑条的高
+			rect.right = rect.left + sliderLength;	//滑条的长
 			slider[i][j].Create(WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, rect, this, IDC_SLIDER_MSG_START + i);
 		}
 	}
+	slider[0][0].SetRange(-180, 180, TRUE);
+	slider[0][1].SetRange(-180, 180, TRUE);
+	slider[0][2].SetRange(-180, 180, TRUE);
+}
+
+void BBA_FlightStatus::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	BBA_CWnd::OnHScroll(nSBCode, nPos, pScrollBar);
+	pScrollBar->GetDlgCtrlID();
+
+	char outPutPos[10];
+	for (int i = 0; i < sliderLineNum; i++)
+	{
+		for (int j = 0; j < sliderListNum; j++)
+		{
+			if(&slider[i][j]==(CSliderCtrl*)pScrollBar)
+			{ 
+				int pos = slider[i][j].GetPos();
+				CPoint point = sliderPoint[i][j];
+				point.y = point.y + sliderWidth / 2-8;	//输出在中间
+				point.x = point.x + sliderLength;	//字长
+				itoa(pos, outPutPos, 10);	//十进制
+				memset(outPutPos + strlen(outPutPos), ' ', 10 - strlen(outPutPos));	//将0后面的填空格
+				outPutPos[9] = 0;	//主动加结束符
+				m_pPDC->TextOut(point.x,point.y, TEXT(outPutPos));
+			}
+		}
+	}
+}
+
+int BBA_FlightStatus::UpdataFlightStatusWindows(char *buff, int len)
+{
+
+
 }
--- Smart_Home/BBA_FlightStatus.h
+++ Smart_Home/BBA_FlightStatus.h
@@ -2,6 +2,7 @@
 #include "BBA_CWnd.h"
 #include "resource.h"
 #include <afxtempl.h>
+#include "BBA_SOCKET.h"
 
 class BBA_FlightStatus :public BBA_CWnd {
 public:
@@ -13,9 +14,11 @@ public:
 	/******************text 相关****************/
 #define STATIC_MAX_LINE_TEXT_NUM	10	//每一行最多有
 #define STATIC_MAX_LIST_TEXT_NUM	10	//每一列
+#define STATIC_MAX_STRLENGTH	30	//最多可以输出30个字符
 
 	CPoint textStartPoint;
 	CPoint textPoint[STATIC_MAX_LINE_TEXT_NUM][STATIC_MAX_LIST_TEXT_NUM];	//TEXT输出的位置
+	char textStr[STATIC_MAX_LINE_TEXT_NUM][STATIC_MAX_LIST_TEXT_NUM][STATIC_MAX_STRLENGTH];
 	int textLineNum;	//每一行的text数目
 	int textListNum;	//每一列的text数目
 	int textLineInterval;	//每一行Text的间隔
@@ -23,7 +26,7 @@ public:
 	/******************************************/
 	/******************Slider相关****************/
 #define STATIC_MAX_LINE_SLIDER_NUM	2	//每一行最多有
-#define STATIC_MAX_LIST_SLIDER_NUM	10	//每一列
+#define STATIC_MAX_LIST_SLIDER_NUM	15	//每一列
 #define SLIDER_NUM	STATIC_MAX_LINE_SLIDER_NUM+STATIC_MAX_LIST_SLIDER_NUM
 #define IDC_SLIDER_MSG_START	3000	//消息ID起始值
 	CPoint sliderStartPoint;	//滑条起点
@@ -32,14 +35,20 @@ public:
 	int sliderListNum;	//每一列的slider数目
 	int sliderLineInterval;	//每一行Slider的间隔
 	int sliderListInterval;	//每一列Slider的间隔
+	int sliderLength;	//滑条长度
+	int sliderWidth;	//滑条宽度
 	CSliderCtrl slider[STATIC_MAX_LINE_SLIDER_NUM][STATIC_MAX_LIST_SLIDER_NUM];
 	/******************************************/
 	CPaintDC *m_pPDC;	//
 	CRect m_RectClientWindow;	//可绘制的区域,由OnPaint消息实时更新
+	BBA_UDP *bba_Station_udp;	//UDP类对象指针
 public:
 	int CreatFlightStatusWindow(CString wndTitle, CRect *rect = NULL, CWnd* pParent = NULL, DWORD id = 0, DWORD style = WS_VISIBLE | WS_CHILD);
+	int UpdataFlightStatusWindows(char *buff);
+	
 public:
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg void OnPaint();
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
 };
